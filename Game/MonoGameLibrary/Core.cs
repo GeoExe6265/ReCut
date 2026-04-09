ff85060872fa0a035bf855198d8fe9a6184ed9bf
GeoExe6265 <karakiyan2007@mail.ru>
2026-04-09 22:18:09 +0500

    Added Game Map

--- Game/MonoGameLibrary/Core.cs
+++ Game/MonoGameLibrary/Core.cs
@@ -79,13 +79,12 @@ public class Core : Game
 
     protected override void Initialize()
     {
-        base.Initialize();
-
         // Set the core's graphics device to a reference of the base Game's
         // graphics device.
         GraphicsDevice = base.GraphicsDevice;
 
         // Create the sprite batch instance.
         SpriteBatch = new SpriteBatch(GraphicsDevice);
+        base.Initialize();
     }
 }
--- Game/ReCut/Game1.cs
+++ Game/ReCut/Game1.cs
@@ -2,6 +2,9 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using MonoGameLibrary;
+using MonoGame.Extended.Tiled;
+using MonoGame.Extended.Tiled.Renderers;
+using System.Collections.Generic;
 
 namespace ReCut;
 
@@ -9,9 +12,18 @@ public class Game1 : Core
 {
     private KeyboardState _previousState;
     private Texture2D _character;
+    private TiledMap _map;
+    private TiledMapRenderer _mapRenderer;
+    private List<Rectangle> _collisionRectangles = new List<Rectangle>();
+    private Vector2 _pos;
+    private Vector2 _velocity;
+    private bool _onGround;
+    private const float Gravity = 1600f;
+    private const float JumpForce = -600f;
+    private const float Speed = 350f;
     public Game1() : base("ReCut", 1280, 720, false)
     {
-        Content.RootDirectory = "Content/images";
+        Content.RootDirectory = "Content";
         IsMouseVisible = true;
     }
 
@@ -38,9 +50,26 @@ public class Game1 : Core
 
     protected override void LoadContent()
     {
-
+        base.LoadContent();
         // TODO: use this.Content to load your game content here
-        _character = Content.Load<Texture2D>("character");
+        _character = Content.Load<Texture2D>("images/character");
+        _map = Content.Load<TiledMap>("maps/level1");
+        _mapRenderer = new TiledMapRenderer(GraphicsDevice, _map);
+
+        var collisionLayer = _map.GetLayer<TiledMapObjectLayer>("Collision");
+        foreach (var obj in collisionLayer.Objects)
+        {
+            if (obj.Name == "Spawn")
+            {
+                _pos = obj.Position;   
+            }
+
+            else
+            {
+                _collisionRectangles.Add(new Rectangle((int)obj.Position.X, (int)obj.Position.Y, (int)obj.Size.Width, (int)obj.Size.Height));
+            }
+
+        }
     }
 
     protected override void Update(GameTime gameTime)
@@ -68,14 +97,10 @@ public class Game1 : Core
         GraphicsDevice.Clear(Color.CornflowerBlue);
 
         // TODO: Add your drawing code here
+        _mapRenderer.Draw();
+
         SpriteBatch.Begin();
-        SpriteBatch.Draw(
-        _character,
-        new Vector2(
-            (Window.ClientBounds.Width * 0.5f) - (_character.Width * 0.5f),
-            (Window.ClientBounds.Height * 0.5f) - (_character.Height * 0.5f)),
-        Color.White
-        );
+        SpriteBatch.Draw(_character, _pos, Color.White);
         SpriteBatch.End();
 
         base.Draw(gameTime);
